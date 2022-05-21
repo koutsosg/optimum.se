f35a445df716a89d8b8e30d9a7a439ea79ec4c93
koutsosg <g.koutsos123@gmail.com>
2022-05-21 18:09:22 +0200

    Back To Top Button added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import MessengerCustomerChat from "react-messenger-customer-chat"
-
+import BackToTop from "../components/Backtotop"
 const Layout = ({ children }) => {
     return (
       <>
         {children}
         <MessengerCustomerChat pageId="2106496549385767" />  
+        <BackToTop  />
       </>
     )
   }
