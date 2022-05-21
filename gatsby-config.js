c4c90ccadd22253b67a73c9df7495db46f441524
koutsosg <g.koutsos123@gmail.com>
2022-05-21 16:41:03 +0200

    gatsby install

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,41 @@
 module.exports = {
   siteMetadata: {
     title: `optimumab.se`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://optimumab.se`
   },
-  plugins: [{
+  plugins: [/* {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": ""
+      "trackingId": "G-DJF6Q5SDEJ"
     }
+  }, */
+  {
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-DJF6Q5SDEJ", // Google Analytics / GA
+        /*    "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager) */
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIdsg
+
+      /*     gtagConfig: {
+        optimize_id: "OPT_CONTAINER_ID",
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      }, */
+    },
   }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
